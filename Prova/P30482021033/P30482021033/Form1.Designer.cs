b092997c1d85581ed438169398cf6cce0bfa87ec
cassiohirota <41903845+cassiohirota@users.noreply.github.com>
2022-12-21 10:29:15 -0300

    Add files via upload

---
+++ Prova/P30482021033/P30482021033/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace P30482021033
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.Lstbx = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(137, 156);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(154, 135);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Lstbx
+            // 
+            this.Lstbx.FormattingEnabled = true;
+            this.Lstbx.ItemHeight = 16;
+            this.Lstbx.Location = new System.Drawing.Point(454, 22);
+            this.Lstbx.Name = "Lstbx";
+            this.Lstbx.Size = new System.Drawing.Size(334, 404);
+            this.Lstbx.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Lstbx);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox Lstbx;
+    }
+}
+
---
+++ Prova/P30482021033/P30482021033/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P30482021033
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+
+            string valor = Interaction.InputBox("Digite o RA", "Entrada de dados");
+
+            char[] numero = valor.ToCharArray();
+            Array.Reverse(numero);
+            int ra = int.Parse(numero[0].ToString());
+
+            if (ra == 0)
+            {
+                ra = 10;
+            }
+
+            string[] mes = new string[ra];
+            double[,] semana = new double[mes.Length, 4];
+            double[] totalmes = new double[mes.Length];
+
+
+            double totalGeral = 0;
+            string auxiliar = "";
+
+            for (int i = 0; i < semana.GetLength(0); i++)
+            {
+                totalmes[i] = 0;
+                for (int j = 0; j < semana.GetLength(1); j++)
+                {
+                    auxiliar = Interaction.InputBox("Digite o valor do mês " + (i + 1).ToString() + " da semana " + (j + 1).ToString(), "Entrada de dados");
+
+                    if (!double.TryParse(auxiliar, out semana[i, j]))
+                    {
+                        MessageBox.Show("Valor Inválido");
+                        j--;
+                        continue;
+                    }
+
+                    if (semana[i, j] < 0)
+                    {
+                        MessageBox.Show("Valor Inválido");
+                        j--;
+                        continue;
+                    }
+
+                    totalmes[i] = totalmes[i] + semana[i, j];
+                }
+
+                totalGeral = totalGeral + totalmes[i];
+            }
+
+            for (int i = 0; i < semana.GetLength(0); i++)
+            {
+                for (int j = 0; j < semana.GetLength(1); j++)
+                {
+                    Lstbx.Items.Add($"Total do mês: {i+1} Semana: {j+1} {String.Format("{0:C2}", semana[i, j])}. \n");
+                }
+
+                Lstbx.Items.Add($">> Total Mês: {String.Format("{0:C2}", totalmes[i])} \n");
+                Lstbx.Items.Add($"------------------------------------ \n");
+            }
+
+            Lstbx.Items.Add($">> Total Geral: {String.Format("{0:C2}", totalGeral)} \n");
+
+        }
+    }
+}
